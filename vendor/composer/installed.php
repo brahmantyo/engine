e19010a05421a4149db65e582c5a8dbefd17f8cf
bram <bram@email.id>
2021-11-28 20:36:46 +0700

    Initialized

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fe190abe06cd1c92aef94505371a0adcacde5cb4',
+    'name' => 'bram/engine',
+  ),
+  'versions' => 
+  array (
+    'bram/engine' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fe190abe06cd1c92aef94505371a0adcacde5cb4',
+    ),
+  ),
+);
